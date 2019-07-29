a1ea244e69b00c54fceebefafb084f6a8f80fbd5
VolodymyrPst <pasternakv19@gmail.com>
2019-07-29 20:51:14 +0300

    laast

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = (state = 0, action) => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	switch (action.type) {
+		case 'inc':
+			return state + 1;
+
+		case 'dec':
+			return state - 1;
+
+		case 'rnd':
+			return state + action.payload;
+
+		default :
+			return state;
+	}
+};
+
+const store = createStore(reducer);
+
+const inc = () => ({type: 'inc'});
+const dec = () => ({type: 'dec'});
+const rnd = (payload) => ({type: 'rnd', payload});
+
+
+document
+	.getElementById('inc')
+	.addEventListener('click', () => {
+		store.dispatch(inc());
+});
+
+document
+	.getElementById('dec')
+	.addEventListener('click', () => {
+		store.dispatch(dec());
+});
+
+document
+	.getElementById('rnd')
+	.addEventListener('click', () => {
+		const payload = Math.floor(Math.random()*11);
+		store.dispatch(rnd(payload));
+});
+
+const update = () => {
+	document
+	.getElementById('counter')
+	.innerHTML = store.getState();
+};
+
+store.subscribe(update);
